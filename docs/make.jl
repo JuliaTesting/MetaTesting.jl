5f3d949256a0db3172563e52c80f8ffb8ee45334
Seth Axen <seth@sethaxen.com>
2023-05-22 09:52:06 +0200

    Rename org to JuliaTesting

--- docs/make.jl
+++ docs/make.jl
@@ -3,10 +3,10 @@ using Documenter
 
 makedocs(;
     modules=[MetaTesting],
-    repo="https://github.com/sethaxen/MetaTesting.jl/blob/{commit}{path}#{line}",
+    repo="https://github.com/JuliaTesting/MetaTesting.jl/blob/{commit}{path}#{line}",
     sitename="MetaTesting.jl",
     format=Documenter.HTML(; prettyurls=get(ENV, "CI", "false") == "true", assets=String[]),
     pages=["Home" => "index.md"],
 )
 
-deploydocs(; repo="github.com/sethaxen/MetaTesting.jl.git", devbranch="main")
+deploydocs(; repo="github.com/JuliaTesting/MetaTesting.jl.git", devbranch="main")
